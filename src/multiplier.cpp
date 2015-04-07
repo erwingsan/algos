f79087ec74f36bae46ae09083671b0ab26a720b0
Erwing Sanchez <erwing.sanchez@amadeus.com>
2015-04-07 17:41:08 +0200

    Add reverseword

--- src/multiplier.cpp
+++ src/multiplier.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 string multiply(string num1, string num2);
 
-int main() {
+int mainM() {
 
 //get strings
 
---
+++ src/reverseWord.cpp
@@ -0,0 +1,46 @@
+// Example program
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void reverseWords(string &s);
+
+int main() {
+
+//get strings
+
+	string str;
+
+	cout << "Enter string: ";
+	getline(cin, str);
+	cout << str << endl;
+
+	reverseWords(str);
+
+	cout << "Res: " << str;
+
+}
+
+void reverseWords(string &s) {
+
+	string res;
+	int len=0;
+
+	for(int i=s.size()-1; i>=0; --i){
+		if(s[i] == ' ' || i==0){
+			int st = i==0? i:i+1;
+			len = i==0? len+1:len;
+			string add = s.substr(st,len);
+			add = i==0? add:add+' ';
+			res += add;;
+			len=0;
+		}
+		else{
+			len++;
+		}
+	}
+
+	s = res;
+
+}
